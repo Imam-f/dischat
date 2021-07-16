73640a737158475c0f68a01e505645cb3fb5924f
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-07-17 00:16:38 +0700

    Create project structure

---
+++ src/Chat/ChatRoom.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ChatRoom() {
+    return (<>
+        About
+    </>)
+}
+
+export default ChatRoom;
---
+++ src/Chat/RoomSelector.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function RoomSelector() {
+    return (<>
+        About
+    </>)
+}
+
+export default RoomSelector;
