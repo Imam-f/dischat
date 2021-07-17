e09fe8625670f2c2c437b620d306e922bbfef34d
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-07-18 00:37:36 +0700

    Edit Slide Content

--- src/Slide/slide.tsx
+++ src/Slide/slide.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Slide(prop:any) {
     if(prop.num == 1) {
-        return <>Content {prop.num}</>
+        return <>Content {prop.num} <br/></>
     } else if (prop.num == 2) {
-        return <>Content {prop.num}</>
+        return <>Content {prop.num} <br/></>
     } else {
-        return <>Content {prop.num}</>
+        return <>Content Zero <br/></>
     }
 }
 
