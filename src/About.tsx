0f12eb4e57a0b17f32ef3c9289154484232ff1e5
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-07-17 20:51:15 +0700

    Edit About

--- src/About.tsx
+++ src/About.tsx
@@ -3,6 +3,27 @@ import React from "react";
 function About() {
     return <>
         About
+        <div>
+            <h1><img src="" alt="" /> Dischat</h1>
+            <h2>Mohamad Imam Firdaus</h2>
+            <div>
+                <p><img src="" alt="" /> email : mohamadimamfirdaus17@gmail.com</p>
+                <p><img src="" alt="" /> git : imam-f</p>
+                <div>
+                    <h3>Library used</h3>
+                    <ol>
+                        <li><img src="" alt="" />Typescript</li>
+                        <li><img src="" alt="" />Skeleton</li>
+                        <li><img src="" alt="" />ReactJS</li>
+                        <li><img src="" alt="" />React Router</li>
+                        <li><img src="" alt="" />Redux-local-storage</li>
+                        <li><img src="" alt="" />PostgreSQL</li>
+                        <li><img src="" alt="" />Websocket</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
     </>
 }
 
