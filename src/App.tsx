b1424e775120858e423849d30ce2e994efbac184
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-07-17 21:59:47 +0700

    Route now functioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {BrowserRouter as Router, Switch, 
     Route, Link} from "react-router-dom";
+import {w3cwebsocket as websocket} from "websocket";
 
 import Chat from './Chat';
 import Help from './Help';
@@ -8,6 +9,8 @@ import About from './About';
 
 import logo from './logo.svg';
 
+
+const ws = new websocket("ws://localhost:8081");
 
 function App() {
     const [count, setCount] = useState(0)
@@ -23,9 +26,9 @@ function App() {
         </div>
             
         <Switch>
-            <Route path="/"><Chat/></Route>
             <Route path="/help"><Help/></Route>
             <Route path="/about"><About/></Route>
+            <Route path="/"><Chat/></Route>
         </Switch>
         </Router>
     </>
