b79c659a0df6630d39d3a1a67f8ac161c3effb98
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-07-17 22:39:42 +0700

    Expandable Help

--- src/Help.tsx
+++ src/Help.tsx
@@ -3,25 +3,37 @@ import Slide from "./Slide/Slide";
 
 function Help() {
     const [page, setPage] = useState(1);
+    const [isExpandPage, setExpandPage] = useState(false);
     const addone = () => {
-        setPage((page+1)%3)
+        setPage((page+1)%3);
     }
     const subone = () => {
         setPage((page+2)%3)
     }
-    
+    const expand = () => {
+        let notExpand = !isExpandPage;
+        setExpandPage(notExpand);
+    }
+    let slides = Array.from(Array(3).keys());
+
     return <>
-        Help
+        Help {slides}
         <div>
             <div onClick={subone}>prev</div>
             <div>
-                <Slide num={page}/>
+                {isExpandPage ? slides.map(
+                        (item, index) => {
+                            return <><Slide num={item}/><br/></>
+                        }
+                        ) : <Slide num={page}/>
+                }
             </div>
             <div onClick={addone}>next</div>
-            <div>down</div>
+            <div onClick={expand}>down</div>
         </div>
     </>
 }
+
 
 // slide show format
 // enter > mkroom > chat > quit
