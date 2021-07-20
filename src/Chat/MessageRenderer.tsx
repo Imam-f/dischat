74b7cc83b41d697535c0e6e74088314dc0284cae
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-07-21 01:29:26 +0700

    Add auto scroll

--- src/Chat/MessageRenderer.tsx
+++ src/Chat/MessageRenderer.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { useritem } from "../type/useritem";
+import React, {useEffect, useRef} from "react";
 
 function MessageRenderer(prop: any) {
+    const bottom = useRef();
+    useEffect(() => {
+        bottom.current.scrollIntoView({behavior: "smooth"});
+    })
     return <>
         {(prop.message == undefined) || (console.log("conditional",prop.message)) ? 
             <div>No message</div> :
             prop.message.map((msg : any, key:any) => {
                 console.log("to show",msg);
                 let isMe = msg.sender == prop.user.name ? "msgself" : "";
-                return <div key={key} className={isMe}>{msg.text} + {msg.sender} + {msg.time}</div>;
+                return <div key={key} className={isMe}>
+                                    <b>{msg.sender}</b><br/>
+                                    <h4>{msg.text}</h4>
+                                    {msg.time}</div>;
             })
         }
+        <span ref={bottom}></span>
     </>
 }
 
